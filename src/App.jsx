d163da99fe7d507382bff819d221327b547ed6de
Haupt04 <116957227+Haupt04@users.noreply.github.com>
2025-07-18 09:52:49 +0200

    Tailwind Set Up in the Project

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      {/* Below is the tailwind tester line to confirm its working */}
+      <h1 className='text-3xl font-bold underline text-red-600'>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
