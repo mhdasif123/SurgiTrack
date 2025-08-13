1700e65fa89be32129a3f92eee6eca7f1c572003
mostafa <mostafaelmoalem@gmail.com>
2025-08-13 19:29:33 +0300

    Fixed header design and buttons, protected routes with PrivateRoute and added a Public Dashboard button.

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -145,7 +145,7 @@ const Header = () => {
 
             <button
               onClick={handleLogout}
-              className="px-4 py-2 bg-red-500 text-white rounded-md text-md font-semibold shadow-md hover:bg-red-600 transition-colors"
+              className="px-4 py-2 bg-(--color-danger) text-white rounded-md text-md font-semibold shadow-md hover:bg-red-600 transition-colors"
             >
               Logout
             </button>
